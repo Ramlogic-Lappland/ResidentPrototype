e3039114d89eefe5ae981212ff357b79c7269a87
Stan <b4d4ssg04t@gmail.com>
2026-08-13 13:08:38 -0300

    Project start
    Added Character movement and rotation, added 2 hitbox zones for future camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float turnSpeed;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        Vector3 movDir;
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        movDir = transform.forward * Input.GetAxis("Vertical") * playerSpeed;
+        controller.Move(movDir * Time.deltaTime - Vector3.up * 0.1f );
+    }
+}
